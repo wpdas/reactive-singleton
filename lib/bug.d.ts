cb10080a5ef5e639e57822a2174a7edc11e176fd
Wenderson Pires <wendersonpdas@gmail.com>
2020-07-21 02:04:01 -0300

    clean

--- lib/bug.d.ts
+++
@@ -1,2 +0,0 @@
-/// <reference types="react" />
-export declare const Bug: () => JSX.Element;
--- lib/use-foo.d.ts
+++
@@ -1,5 +0,0 @@
-export declare function useFoo<C>(singletonClass: C): {
-    singleton: C;
-    singletonStatus: import("./types").Status;
-    managerStatus: import("./types").Status;
-};
