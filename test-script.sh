57607988c46c40889bc644b310dc3b9628f36682
Wenderson Pires <wendersonpdas@gmail.com>
2020-08-23 02:07:45 -0300

    test-script.sh file to run the tests

--- test-script.sh
+++ test-script.sh
@@ -6,3 +6,16 @@ cp ../package_test.json ./package.json &&
 yarn install &&
 cd node_modules/react &&
 yarn link &&
+cd ../react-dom &&
+yarn link &&
+cd ../../../ &&
+yarn link react &&
+yarn link react-dom &&
+yarn test &&
+yarn unlink react && yarn unlink react-dom &&
+cd temp/node_modules/react &&
+yarn unlink &&
+cd ../react-dom &&
+yarn unlink &&
+cd ../../../ &&
+rm -rf temp
